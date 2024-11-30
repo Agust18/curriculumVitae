7741e2f85df1712dba65754acd8a47be3dabbb2e
Agust18 <agustincamera16@hotmail.com>
2024-11-30 20:42:52 -0300

    Primer commit

---
+++ first curriculum project/educacion.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    
+    <title>educacion</title>
+</head>
+<body>
+<header>
+    <p class="nombre-educacion">Educacion</p>
+    <nav class="nav-links">
+        <ul class="nav-links">
+            <li><a href="index.php">Sobre mi</a></li>
+            <li><a href="educacion.php">Educacion</a></li>
+            <li><a href="capacidades.php">Capacidades</a></li>
+            <li><a href="contacto.php">Contacto</a></li>
+        </ul>
+    </nav>
+</header>
+<div class="contenedor">
+    <div class="finalizados">
+        <h2>Finalizados</h2>
+        <p>
+            Estudie mi bachiller en la Secundaria Nacional Numero N°4 con mi egreso en 2018.
+        </p>
+        <p>
+            Tecnicatura en Analista en Sistemas(2020–2023)<br>
+            cursada finalizada.
+            Finales pendientes
+        </p>
+    </div>
+    <div class="cursando">
+        <h2>Cursando</h2>
+        <p>
+            Actualmente Tecnicatura en Programacion. Utn aula Chacabuco (2024- actualmente)
+        </p>
+    </div>   
+
+    
+</div>
+
+
+
+    
+</body>
+</html>
---
+++ first curriculum project/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="index.css">
+    
+    <title>index</title>
+
+</head>
+<body>
+<header>
+  
+   
+    <h1>Curriculum Vitae</h1>
+    <p class="nombre">Agustin Camera</p>
+    <nav class="nav-links">
+        <ul class="nav-links">
+            <li><a href="index.php ">Sobre mi</a></li>
+            <li><a href="educacion.php">Educacion</a></li>
+            <li><a href="capacidades.php">Capacidades</a></li>
+            <li><a href="contacto.php">Contacto</a></li>
+        </ul>
+    </nav>
+</header>
+<div class="contenedor">
+    <div class="div-sobre-mi">
+        <div class="contenedor-img">
+            <img src="thumb-1920-1020808.jpg" alt="foto">
+        </div>
+        <p class="texto-sobre-mi">
+            Mi nombre es Agustín, tengo 23 años.<br>
+            Nací en Chacabuco,Provincia de Buenos Aires.<br>
+            Me considero una persona educada,amable,que siempre tiene ganas de aprender cosas nuevas,capaz de trabajar en equipo y formar relaciones nuevas.<br>
+            Busco experiencia laboral en cualquier campo,rubro u ámbito con el fin de adquirir nuevas habilidades laborales.<br>
+            Poseo buen manejo de pc y facilidad de aprender todo lo relacionado.<br>
+            Buen compañero concapacidad para trabajar de forma conjunta.
+            Me gusta la sensacion de saber que estoy mejorando personalmente en cualquier aspecto y que con eso ayudo a los demas.<br>
+            Me relaciono bien en el ambito informatico y capacidad para aprender todo lo relacionado.
+        </p>
+    </div>
+
+    <div class="div-info-title">
+        <P>Informacion adicional</P>
+        <ul>
+            <li>Nacionalidad: Argentino</li>
+            <li>Estado Civil: Soltero</li>
+            <li>Carnet de Conducir: Tipo AB</li>
+            <li>Disponibilidad horaria: Amplia</li>
+        </ul>
+
+        
+    </div>
+    
+</div>
+
+
+
+
+
+</body>
+</html>
